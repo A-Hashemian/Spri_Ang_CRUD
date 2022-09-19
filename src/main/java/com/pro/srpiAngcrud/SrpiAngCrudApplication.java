97769074b1907a69e50b2c86e0ba9f6655e09696
A-Hashemian <amin@hashemian.tech>
2022-09-19 23:43:23 +0300

    spring starter

---
+++ src/main/java/com/pro/srpiAngcrud/SrpiAngCrudApplication.java
@@ -0,0 +1,13 @@
+package com.pro.srpiAngcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrpiAngCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrpiAngCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pro/srpiAngcrud/SrpiAngCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pro.srpiAngcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrpiAngCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
